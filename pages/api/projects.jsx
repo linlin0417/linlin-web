e13c7126af047e29ced53c4811b58272572ddcad
linlin Saya <linlin0417022@gmail.com>
2025-03-25 13:12:29 +0800

    暫時

--- pages/api/projects.jsx
+++ pages/api/projects.jsx
@@ -17,9 +17,23 @@ export default async (req, res) => {
 		{
 			id: 3,
 			image: "https://github.com/Tdr-gaming-studio/gitaenobot/blob/v2/docs/img/GItaENObot.png?raw=true",
-			name: "GitaENObot開源版",
+			name: "GitaENObot開源版(v2)",
 			description: "一個開源的Discordbot - 可以簡單的提供基本服務",
 			link: "https://github.com/Tdr-gaming-studio/gitaenobot/tree/v2",
+		},
+		{
+			id: 4,
+			image: "https://github.com/Tdr-gaming-studio/gitaenobot/blob/v2/docs/img/GItaENObot.png?raw=true",
+			name: "GitaENObot開源版(v3)",
+			description: "一個開源的Discordbot - 具有多項功能",
+			link: "https://github.com/Tdr-gaming-studio/gitaenobot/tree/v3",
+		},
+		{
+			id: 5,
+			image: "https://github.com/Tdr-gaming-studio/gitaenobot/blob/v2/docs/img/GItaENObot.png?raw=true",
+			name: "tdr簡易資訊計畫api",
+			description: "可以用於快速獲得資訊的api",
+			link: "https://botapiserver.vercel.app/",
 		}
 	];
 	res.status(200).json(data);
